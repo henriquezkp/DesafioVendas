573f5586285e1f6bc0a2c4f80ed4bccba5a7a660
Lucas Kunz <lucaskunz1@gmail.com>
2021-03-24 20:57:24 -0300

    Migration Movimentação

--- src/database/migrations/20210324231321-create-table-movimentacao.js
+++ src/database/migrations/20210324231321-create-table-movimentacao.js
@@ -2,20 +2,50 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('movimentacoes', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      tipo: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      quantidade: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      id_Estoque: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'estoques',
+          key: 'id'
+        },
+      },
+      id_Produto: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'produtos',
+          key: 'id'
+        },
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+
+    });
+
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('movimentacoes');
   }
 };
