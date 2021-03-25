89a355eceb603fac15e32c44342c8eba95eeb456
Bruno305Rodrigues <bruno_lmr_op@hotmail.com>
2021-03-24 21:29:32 -0300

    finalizando tabela estoque

--- src/database/migrations/20210324231329-create-table-estoque.js
+++ src/database/migrations/20210324231329-create-table-estoque.js
@@ -12,7 +12,15 @@ module.exports = {
        nome: {
         type: Sequelize.STRING(100),
         allowNull: false
-       }
+       },
+       created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
 
 
        });
