b0e97f52189717044293bb852c5eb04eb92a2343
Lucas Kunz <lucaskunz1@gmail.com>
2021-03-24 21:00:53 -0300

    Migration Devolução

--- src/database/migrations/20210324231358-create-table-devolucao.js
+++ src/database/migrations/20210324231358-create-table-devolucao.js
@@ -2,20 +2,52 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('devolucoes', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      id_Estoque: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'estoques',
+          key: 'id'
+        },
+      },
+      id_Produto: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'produtos',
+          key: 'id'
+        },
+      },
+      id_Motivo: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'motivos',
+          key: 'id'
+        },
+      },
+      quantidade: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('devolucoes');
   }
 };
