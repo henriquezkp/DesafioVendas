85b1f6bf9b0f83979607141c572cd2e641509421
Henrique Zanotti <henriquezk.p@hotmail.com>
2021-03-25 21:24:36 -0300

    seeds

--- src/app/controllers/categoriaController.js
+++ src/app/controllers/categoriaController.js
@@ -1,8 +1,17 @@
 import sequelize from 'sequelize';
+import Produto from '../models/produto';
+import Categoria from './../models/categoria';
 
 class CategoriaController {
     async index(req, res) {
-        res.json('ok');
+        const categorias = await Categoria.findAll({
+            include:[
+                {
+                    model: Produto
+                }
+            ]
+        });
+        return res.status(200).json(categorias);
     }
 
 }
